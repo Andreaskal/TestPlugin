3985d9c3fce6c92b365211ba04b731ba7831e3f1
BuildTools <unconfigured@null.spigotmc.org>
2018-05-13 18:47:26 +0200

    Change due to the basic command

--- src/main/java/io/github/Andreaskal/testplugin/TestPlugin.java
+++ src/main/java/io/github/Andreaskal/testplugin/TestPlugin.java
@@ -29,15 +29,7 @@ public final class TestPlugin extends JavaPlugin{
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
     	if (cmd.getName().equalsIgnoreCase("basic")) { // If the player typed /basic then do the following...
-    		// do something...
-    		return true;
-    	} else if (cmd.getName().equalsIgnoreCase("basic2")) {
-    		if (!(sender instanceof Player)) {
-    			sender.sendMessage("This command can only be run by a player.");
-    		} else {
-    			Player player = (Player) sender;
-    			// do something
-    		}
+    		generateCube(null, 0);
     		return true;
     	}
     	return false;
